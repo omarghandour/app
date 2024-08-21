9463fdb26b9b595909a4ab9ba58e57d7250e39a8
omarghandour <alex.forget50@gmail.com>
2024-08-21 18:13:52 +0300

    ws refactor

--- routes/wsRoutes.ts
+++ routes/wsRoutes.ts
@@ -1,7 +1,11 @@
 import Elysia, { t } from "elysia";
 import Task from "../models/TaskModel";
 import jwt from "@elysiajs/jwt";
+import mongoose from "mongoose";
+
 const tokensec: any = process.env.JWT_SECRET;
+
+const userSockets = new Map<string, any>(); // Map to store userId and their WebSocket connection
 
 export const websocket = new Elysia({ prefix: "/ws" }).use(
   jwt({
@@ -11,14 +15,47 @@ export const websocket = new Elysia({ prefix: "/ws" }).use(
   })
 );
 
-websocket.ws("/", {
-  async message(ws, message: any) {
-    const userID = message.id;
+websocket.ws("/:id", {
+  async open(ws) {
+    // Store WebSocket connections based on verified user ID
+    const userID = ws.data.params.id;
+    const verifyID: any = await ws.data.jwt.verify(userID);
+    // console.log(verifyID);
+
+    userSockets.set(verifyID.id, ws);
+  },
+  async message(ws) {
+    const userID = ws.data.params.id;
     const verifyID: any = await ws.data.jwt.verify(userID);
     const ID: any = verifyID.id;
     const unreadTasks = await Task.find({ assignedTo: ID, read: false });
-    console.log(unreadTasks);
 
-    ws.send(unreadTasks.length);
+    ws.send({ unreadCount: unreadTasks.length });
   },
+  async close(ws) {
+    const userID = ws.data.params.id;
+    const verifyID: any = await ws.data.jwt.verify(userID);
+
+    // console.log(userID);
+
+    userSockets.delete(verifyID); // Remove the WebSocket connection on close
+  },
+});
+
+// Watch for changes in the Task collection
+const taskChangeStream = Task.watch();
+
+taskChangeStream.on("change", async (change) => {
+  // if (change.operationType === "insert" || change.operationType === "update") {
+  const { assignedTo } = change.fullDocument;
+
+  // Find unread tasks for the user whose task was changed
+  const unreadTasks = await Task.find({ assignedTo, read: false });
+
+  // Send the update to the relevant WebSocket client
+  const client = userSockets.get(assignedTo);
+  if (client) {
+    client.send(JSON.stringify({ unreadCount: unreadTasks.length }));
+  }
+  // }
 });
